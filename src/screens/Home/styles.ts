50c737e1150c2a5d1293a3f0722aef9daea07984
marcelorpc <marcelorpchaves@gmail.com>
2024-03-19 11:43:08 -0300

    create form styles

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,13 +1,43 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  headerContainer: {
+  container: {
+    flex: 1,
+  },
+
+  header: {
+    height: 173,
     padding: 24,
-    backgroundColor: '#0D0D0D',
-    height: 170
+    backgroundColor: "#0D0D0D",
   },
   logo: {
     alignSelf: "center",
     marginTop: 40
+  },
+  form: {
+    flexDirection: "row",
+    paddingHorizontal: 24,
+    gap: 4,
+    transform: [{translateY: -30}]
+  },
+  input: {
+    flex: 1,
+    height: 54,
+    padding: 16,
+    backgroundColor: "#262626",
+    borderRadius: 6,
+    color: "#F2F2F2",
+    fontSize: 16
+  },
+  button: {
+    width: 52,
+    height: 52,
+    borderRadius: 6,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#1E6F9F"
+  },
+  buttonText: {
+    color: "#F2F2F2"
   }
 })
