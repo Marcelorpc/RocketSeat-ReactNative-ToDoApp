f9acfe4e1b0b6649a70df0e38bdda3cd0bcde4d4
marcelorpc <marcelorpchaves@gmail.com>
2024-03-19 10:58:29 -0300

    create header styles

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  headerContainer: {
+    padding: 24,
+    backgroundColor: '#0D0D0D',
+    height: 170
+  },
+  logo: {
+    alignSelf: "center",
+    marginTop: 40
+  }
+})
