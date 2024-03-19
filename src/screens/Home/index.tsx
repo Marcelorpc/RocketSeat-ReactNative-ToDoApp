2925989c0b4ed9b2dcffe1e40cf3500e4ce04ad8
marcelorpc <marcelorpchaves@gmail.com>
2024-03-19 11:42:52 -0300

    add form input and button

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,29 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
 
 export default function Home() {
   return (
-    <View style={styles.headerContainer}>
-      <Image
-        style={styles.logo}
-        source={require('../../../assets/Logo.png')}
-      />
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image
+          style={styles.logo}
+          source={require('../../../assets/Logo.png')}
+        />
+      </View>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Crie uma nova tarefa"
+          placeholderTextColor="#808080"
+        />
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>
+            +
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
